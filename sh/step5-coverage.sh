8d88a7cbe733d119019c6aa7e89ead598e66d11e
lcolladotor <lcollado@jhsph.edu>
2016-12-19 13:42:54 -0500

    Bump coverage step mem

--- sh/step5-coverage.sh
+++ sh/step5-coverage.sh
@@ -16,9 +16,9 @@ sname="${SHORT}.${PREFIX}"
 
 if [[ $LARGE == "TRUE" ]]
 then
-    MEM="mem_free=20G,h_vmem=40G,h_fsize=100G"
+    MEM="mem_free=60G,h_vmem=80G,h_fsize=100G"
 else
-    MEM="mem_free=10G,h_vmem=20G,h_fsize=100G"
+    MEM="mem_free=30G,h_vmem=40G,h_fsize=100G"
 fi
 
 if [ -e ".send_emails" ]
