766a56dcdc3c48a250f24d9e3a016d29c8594241
Emily Burke <emily.burke@libd.org>
2017-01-23 11:51:51 -0500

    Added R code to ERCC section to check for finiteness

--- sh/create_count_objects-human.R
+++ sh/create_count_objects-human.R
@@ -83,6 +83,9 @@ if (ERCC == TRUE ){
 	})
 	rownames(erccTPM) = read.table(file.path(MAINDIR, "Ercc", sampIDs[1], "abundance.tsv"),
 							header = TRUE)$target_id
+	#check finiteness / change columns with all NaNs to 0s
+	erccTPM[,which(is.na(colSums(erccTPM)))] = 0
+	
 	#expected concentration
 	spikeIns = read.delim("/users/ajaffe/Lieber/Projects/RNAseq/Ribozero_Compare/ercc_actual_conc.txt",
 								as.is=TRUE,row.names=2)
