19d6fe6569424eae52bad6797d1889d3d64af05a
Leonardo Collado Torres <lcollado@jhsph.edu>
2017-02-02 15:49:25 -0500

    Drop qualV dependency

--- sh/check_R_packages.R
+++ sh/check_R_packages.R
@@ -2,7 +2,7 @@ packages <- c('Biostrings', 'GenomicRanges', 'GenomicFeatures', 'org.Hs.eg.db',
     'biomaRt', 'BSgenome.Hsapiens.UCSC.hg19', 'BSgenome.Hsapiens.UCSC.hg38',
     'org.Mm.eg.db', 'BSgenome.Mmusculus.UCSC.mm10', 'org.Rn.eg.db',
     'BSgenome.Rnorvegicus.UCSC.rn6', 'derfinder', 'bumphunter', 'jaffelab',
-    'devtools', 'getopt', 'BiocParallel', 'rafalib', 'qualV')
+    'devtools', 'getopt', 'BiocParallel', 'rafalib')
 
 ## Try to load them
 load_res <- sapply(packages, requireNamespace, quietly = TRUE)
