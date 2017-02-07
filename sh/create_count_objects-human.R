b841caac7d96a7246c830cb41f6d30a6af7a016f
Emily Burke <emily.burke@libd.org>
2017-02-07 14:34:43 -0500

    Include erccTPM dataframe in final rda file. Closes #1

--- sh/create_count_objects-human.R
+++ sh/create_count_objects-human.R
@@ -95,7 +95,8 @@ if (opt$ercc == TRUE ){
 						nc = ncol(erccTPM), nr = nrow(erccTPM), byrow=FALSE)
 	logErr = (log2(erccTPM+1) - log2(10*mix1conc+1))
 	metrics$ERCCsumLogErr = colSums(logErr)
-		
+	
+	erccTPM = t(erccTPM)	
 	}
 ############################################################
 
@@ -436,10 +437,20 @@ colnames(mcols(jMap))[10] = "Class"
 
 ### save counts
 
-save(metrics, jMap, jCounts, geneCounts, geneMap, exonCounts, exonMap, compress=TRUE,
+tosaveCounts = c("metrics", "geneCounts", "geneMap", "exonCounts", "exonMap", "jCounts", "jMap" )
+tosaveRpkm = c("metrics", "geneRpkm", "geneMap", "exonRpkm", "exonMap", "jRpkm", "jMap")
+
+if (exists("erccTPM")) {
+	tosaveCounts = c("erccTPM", tosaveCounts)
+	tosaveRpkm = c("erccTPM", tosaveRpkm)
+}
+
+save(list=ls()[ls() %in% tosaveCounts], compress=TRUE,
 	file= file.path(opt$maindir, paste0('rawCounts_', EXPNAME, '_n', N, '.rda')))
-save(metrics, jMap, jRpkm, geneRpkm,	geneMap, exonRpkm, exonMap, compress=TRUE,
+save(list=ls()[ls() %in% tosaveRpkm], compress=TRUE,
 	file= file.path(opt$maindir, paste0('rpkmCounts_', EXPNAME, '_n', N, '.rda')))
+
+
 
 ## write out for coverage
 write.table(metrics[,c("SAMPLE_ID", "bamFile")], 
