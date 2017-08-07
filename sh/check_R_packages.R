cfa430625fd494a7158b0a2952e9ea1bca5b8e89
Leonardo Collado Torres <lcollado@jhsph.edu>
2017-08-07 15:58:06 -0400

    Add plyr to list of R packages checked

--- sh/check_R_packages.R
+++ sh/check_R_packages.R
@@ -2,7 +2,8 @@ packages <- c('Biostrings', 'GenomicRanges', 'GenomicFeatures', 'org.Hs.eg.db',
     'biomaRt', 'BSgenome.Hsapiens.UCSC.hg19', 'BSgenome.Hsapiens.UCSC.hg38',
     'org.Mm.eg.db', 'BSgenome.Mmusculus.UCSC.mm10', 'org.Rn.eg.db',
     'BSgenome.Rnorvegicus.UCSC.rn6', 'derfinder', 'bumphunter', 'jaffelab',
-    'devtools', 'getopt', 'BiocParallel', 'rafalib', 'SummarizedExperiment')
+    'devtools', 'getopt', 'BiocParallel', 'rafalib', 'SummarizedExperiment',
+    'plyr')
 
 ## Try to load them
 load_res <- sapply(packages, requireNamespace, quietly = TRUE)
